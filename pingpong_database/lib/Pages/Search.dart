d78ebb10b30b406c5c115ca99e66376901c8f1e8
dikdok6969 <prinza12345@gmail.com>
2022-04-15 21:48:23 +0700

    you go do json format

---
+++ pingpong_database/lib/Pages/Search.dart
@@ -0,0 +1,157 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:pingpong_database/Pages/TTModel.dart';
+import 'package:pingpong_database/main.dart';
+import 'dart:html';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Table Tennis App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  final dio = Dio();
+  //final List<TTModel> TTdata =
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List? _tabletennis;
+
+  void searchTT(String query) async {
+    final response = await widget.dio.get(
+      "https://4t03f4nk05.execute-api.us-east-2.amazonaws.com/product?",
+      queryParameters: {
+        'name': query,
+      },
+    );
+    //print(response);
+    print(response.data['Items']);
+    /*List ttname = json
+        .decode(response.data['Items']['name']['S'])
+        .map((data) => TTModel.fromJson(data))
+        .toList();
+    ;
+    print(ttname);*/
+    setState(() {
+      _tabletennis = response.data['Items'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            /*ElevatedButton(
+                // Within the `FirstScreen` widget
+                onPressed: () {
+                  // Navigate to the second screen using a named route.
+                  Navigator.popUntil(context, ModalRoute.withName('/Home'));
+                },
+                child: const Text('Home'),
+                style:
+                    ElevatedButton.styleFrom(primary: Colors.yellow.shade600)),*/
+            ResultPage(onSearch: searchTT),
+            _tabletennis == null
+                ? Text("")
+                : Expanded(
+                    child: ListView(
+                      children: _tabletennis!.map((tabletennis) {
+                        return ListTile(
+                          title: Text(tabletennis['name']['S']),
+                          subtitle: Text('speed ${tabletennis['speed']['N']} \n'
+                              'control ${tabletennis['control']['N']}'),
+                          leading: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Image.network(tabletennis['picture']['S']),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ResultPage extends StatefulWidget {
+  ResultPage({required this.onSearch});
+  final void Function(String search) onSearch;
+
+  @override
+  _ResultPageState createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  final _Key = GlobalKey<FormState>();
+  var _search;
+  @override
+  Widget build(context) {
+    return Form(
+      key: _Key,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+            ),
+            onChanged: (value) {
+              _search = value;
+            },
+            validator: (value) {
+              if (value!.isNotEmpty) {
+                return "KUY";
+              }
+              return null;
+            },
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          FlatButton(
+            onPressed: () {
+              widget.onSearch(_search);
+            },
+            color: Colors.blue,
+            child: Text(
+              "Search",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- pingpong_database/lib/main.dart
+++ pingpong_database/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:pingpong_database/Pages/SecondScreen.dart';
 import 'package:pingpong_database/Pages/ThirdScreen.dart';
 import 'package:pingpong_database/Pages/Customize.dart';
+import 'package:pingpong_database/Pages/Search.dart';
 
 void main() {
   runApp(
@@ -12,14 +13,13 @@ void main() {
       title: 'Homepage',
       // Start the app with the "/" named route. In this case, the app starts
       // on the FirstScreen widget.
-      initialRoute: '/',
+      initialRoute: '/Home',
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => const FirstScreen(),
+        '/Home': (context) => const FirstScreen(),
         // When navigating to the "/second" route, build the SecondScreen widget.
         '/Favorite': (context) => const SecondScreen(),
-        '/Search': (context) => ThirdScreen(),
-        '/Customize': (context) => const Customize(),
+        '/Search': (context) => MyApp(),
       },
     ),
   );
@@ -42,14 +42,24 @@ class FirstScreen extends StatelessWidget {
               Text(
                 'Welcome To the PingPong information',
                 style: Theme.of(context).textTheme.headline3,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 30,
               ),
               Image.asset(
                 'images/stiga-allround-classic-wrb.jpg',
                 fit: BoxFit.cover,
               ),
+              SizedBox(
+                height: 30,
+              ),
               Text(
                 'Press to go to add your favorite racket',
                 style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(
+                height: 30,
               ),
               ElevatedButton(
                   // Within the `FirstScreen` widget
@@ -59,7 +69,10 @@ class FirstScreen extends StatelessWidget {
                   },
                   child: const Text('Search'),
                   style: ElevatedButton.styleFrom(
-                      primary: Colors.yellow.shade600)),
+                      primary: Color(0xFF3592FD), fixedSize: Size(100, 25))),
+              SizedBox(
+                height: 10,
+              ),
               ElevatedButton(
                 // Within the `FirstScreen` widget
                 onPressed: () {
@@ -67,16 +80,12 @@ class FirstScreen extends StatelessWidget {
                   Navigator.pushNamed(context, '/Favorite');
                 },
                 child: const Text('Favorite'),
+                style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF3592FD), fixedSize: Size(100, 25)),
               ),
-              ElevatedButton(
-                  // Within the `FirstScreen` widget
-                  onPressed: () {
-                    // Navigate to the second screen using a named route.
-                    Navigator.pushNamed(context, '/Customize');
-                  },
-                  child: const Text('Customize'),
-                  style:
-                      ElevatedButton.styleFrom(primary: Colors.green.shade600)),
+              SizedBox(
+                height: 10,
+              ),
             ],
           ),
         ),
